56975b2efd0d1e9608661322b6051eda9aa1369a
Owen Walton <odwalton07@gmail.com>
2025-05-21 00:43:45 +0100

    Add code for a java RoomBooking model into RoomBooking.java

--- src/main/java/model/RoomBooking.java
+++ src/main/java/model/RoomBooking.java
@@ -1,4 +1,70 @@
+/**
+ * RoomBooking instances store all data provided by the database from RoomBooking table
+ * Accessed by RoomBookingDAO
+ * Used when interacting with RoomBooking table in DB
+ * @author Owen Walton
+ * Last edit: 21/05/25
+ */
+
 package main.java.model;
 
-public class RoomBooking {
+public class RoomBooking
+{
+    // ----------------------------------------------------------------------
+    // Class variables
+    // ----------------------------------------------------------------------
+    // Fields in db-table.
+    private int iRoomBookingID;
+    private int iRoomNumber;
+    private int iBookingId;
+    private short iOccupants;
+
+    // ----------------------------------------------------------------------
+    // Constructor
+    // ----------------------------------------------------------------------
+    public RoomBooking(int iRoomBookingID, int iRoomNumber, int iBookingId, short iOccupants)
+    {
+        this.iRoomBookingID = iRoomBookingID;
+        this.iRoomNumber = iRoomNumber;
+        this.iBookingId = iBookingId;
+        this.iOccupants = iOccupants;
+    }
+
+    // ----------------------------------------------------------------------
+    // Setters
+    // ----------------------------------------------------------------------
+    public void setIRoomBookingID(int iRoomBookingID) {
+        this.iRoomBookingID = iRoomBookingID;
+    }
+
+    public void setIRoomNumber(int iRoomNumber) {
+        this.iRoomNumber = iRoomNumber;
+    }
+
+    public void setIBookingId(int iBookingId) {
+        this.iBookingId = iBookingId;
+    }
+
+    public void setIOccupants(short iOccupants) {
+        this.iOccupants = iOccupants;
+    }
+
+    // ----------------------------------------------------------------------
+    // Getters
+    // ----------------------------------------------------------------------
+    public int getIRoomBookingID() {
+        return iRoomBookingID;
+    }
+
+    public int getIRoomNumber() {
+        return iRoomNumber;
+    }
+
+    public int getIBookingId() {
+        return iBookingId;
+    }
+
+    public short getIOccupants() {
+        return iOccupants;
+    }
 }
